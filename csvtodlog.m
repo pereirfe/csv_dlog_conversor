a35fe3b09dd2b9456f281918c9e9df845c012249
Fernando <fernandhenriqp@gmail.com>
2016-02-16 15:34:55 -0200

    improving script

--- csvtodlog.m
+++ csvtodlog.m
@@ -1,6 +1,6 @@
 mat = csvread("PROCESSED_CSV.csv");
 mat = mat(:,2)';
-[l,c] =size(mat);
+[l,c] = size(mat);
 # save -float-binary dlog.bin mat
 id = fopen("dlog2.bin", 'w');
 fwrite(id,c,  "int64",0,"ieee-be");
--- script.sh
+++ script.sh
@@ -2,20 +2,24 @@
 
 #TODO: Receive name as parameter
 
-#Pre-processing
-period=`(sed -n '2,2 p' TESTCSV.csv | cut -d ':' -f2) | tr -d '\n' | tr -d '\r'`  #Gets the time between samplings
+if [ $# -ne 1 ]; then
+    echo "Usage: "$0" filename.csv"
 
-cp TESTCSV.csv PROCESSED_CSV.csv
-sed -i '1,4d' PROCESSED_CSV.csv
+else
+    #Pre-processing
+    period=`(sed -n '2,2 p' TESTCSV.csv | cut -d ':' -f2) | tr -d '\n' | tr -d '\r'`  #Gets the time between samplings
 
-#Calling Octave for processing
-octave csvtodlog.m
-
-#MountingXML
-cp settings.dlog.init out.dlog
-sed -i 's/@@/'$period'/' out.dlog
-
-cat dlog2.bin >> out.dlog
-
-#mounting file
-cp B.csv TESTCSV.csv
+    cp $1 PROCESSED_CSV.csv
+    sed -i '1,4d' PROCESSED_CSV.csv
+    
+    #Calling Octave for processing
+    octave -q csvtodlog.m
+    
+    #MountingXML
+    cp settings.dlog.init out.dlog
+    sed -i 's/@@/'$period'/' out.dlog
+    
+    cat dlog2.bin >> out.dlog
+    
+    rm -rf PROCESSED_CSV.csv
+fi
